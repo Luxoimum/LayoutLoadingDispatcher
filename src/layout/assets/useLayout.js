8a8691dd3374532d1a8a334a6c0fa1920ba4e8bd
Federico <federico.pereyra@upm.es>
2020-05-08 18:09:44 +0200

    [Refactor] reorder the files and preparing repo to start a demo in local

--- src/layout/assets/useLayout.js
+++ src/layout/assets/useLayout.js
@@ -1,6 +1,8 @@
-import { useContext } from 'react'
-import LayoutContext from './LayoutProvider'
+import { useContext } from 'react';
+import LayoutContext from './LayoutProvider';
 
 export default function useLayout() {
-  return useContext(LayoutContext);
+  const context = useContext(LayoutContext);
+  console.log(context);
+  return context;
 }
